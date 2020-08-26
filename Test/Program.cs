205fe4ef11740be3cd6867f52604fe459e3e56ed
Fatma Alhouthi <fa222nx@student.lnu.se>
2020-08-26 15:56:51 +0200

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2};
+            list
+
+            Student stu = new Student("Sam");
+            
+            Console.WriteLine(arr.ToArray());
+        }
+    }
+}
---
+++ Test/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Student
+    {
+        string name;
+
+        public Student(string name)
+        {
+            this.name = name;
+        }
+        public string GetName() { 
+            return name;
+        
+
+        }
+
+    }
+}
